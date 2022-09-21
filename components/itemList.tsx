08c1ee9450b303e3e370c6402b6f5d553a63b2a7
Robert <robert@example.com>
2022-09-21 10:40:39 -0700

    Added Pagination to itemList

--- components/itemList.tsx
+++ components/itemList.tsx
@@ -1,24 +1,38 @@
 import React, { useState } from 'react';
 import Item from './item';
 
-import { Box } from '@mui/material';
+import { Box, Pagination } from '@mui/material';
 import { DatasetContext } from '../dataset/datasetContext';
 
 export default function ItemList() {
   const datasetContext = React.useContext(DatasetContext);
   const setItemGroup = datasetContext.setItemGroup;
 
+  let [itemsPerPage, setItemsPerPage] = useState(6);
+  let [showPagination, setShowPagination] = useState(false);
+  let [currentPageNumber, setCurrentPageNumber] = useState(1);
+  let [pageCount, setPageCount] = useState(0);
+
   let [items, setItems] = useState<React.ReactElement[]>();
 
   React.useEffect(() => {
-    const elements = datasetContext.dataset.map((dataItem) => (
+    const startIndex = itemsPerPage * (currentPageNumber - 1);
+    const endIndex = itemsPerPage * (currentPageNumber);
+
+    const elements = datasetContext.dataset.slice(startIndex, endIndex).map((dataItem) => (
       <React.Fragment key={dataItem.id}>
         <Item data={dataItem} setGroup={setItemGroup}></Item>
       </React.Fragment>
     ));
 
     setItems(elements);
-  }, [datasetContext.dataset, setItemGroup])
+    setShowPagination(datasetContext.dataset.length > itemsPerPage)
+    setPageCount(Math.ceil(datasetContext.dataset.length/itemsPerPage))
+  }, [datasetContext.dataset, setItemGroup, itemsPerPage, currentPageNumber])
+
+  function setPage(page: number) {
+    setCurrentPageNumber(page);
+  }
 
   return (
     <React.Fragment>
@@ -29,6 +43,9 @@ export default function ItemList() {
         }}>
         {items}
       </Box>
+      {showPagination && 
+        <Pagination count={pageCount} page={currentPageNumber} onChange={(e, page) => setPage(page)} />
+      }
     </React.Fragment>
   );
 }
